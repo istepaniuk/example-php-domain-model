938a8f20baca037ccc4e1c45adc6a89af8e9d4d8
Iván Stepaniuk <istepaniuk@gmail.com>
2016-11-09 18:41:35 +0100

    testing phpunit (bad test)

--- test/Newsletter/NewsletterServiceTest.php
+++ test/Newsletter/NewsletterServiceTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Newsletter\Domain\Clock;
+use Newsletter\Domain\Newsletter;
+use Newsletter\Domain\NewsletterSender;
 use Newsletter\Domain\NewsletterService;
+use Newsletter\Domain\SubscriberRepository;
 
 class NewsletterServiceTest extends PHPUnit_Framework_TestCase
 {
@@ -9,9 +13,13 @@ class NewsletterServiceTest extends PHPUnit_Framework_TestCase
      */
     public function doesNotSendNewslettersIfThereAreNoSubscribers()
     {
-        $sender = $this->createMock(NewsletterService::class);
-        $clock = $this->createMock(NewsletterService::class);
-        $sender = $this->createMock(NewsletterService::class);
+        $repository = $this->createMock(SubscriberRepository::class);
+        $clock = $this->createMock(Clock::class);
+        $sender = $this->createMock(NewsletterSender::class);
+
         $service = new NewsletterService($repository, $clock, $sender);
+
+        $newsletter = new Newsletter("A test", "Testing...");
+        $service->sendNewsletterToAllSubscribers($newsletter);
     }
 }
